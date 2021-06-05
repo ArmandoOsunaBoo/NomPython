ec447b670cc3351b94ff994e0d9d85ea78dfa67b
ArmandoOsunaBoo <creatorscave123@gmail.com>
2021-06-05 08:00:13 -0500

    Version 1.0.4

--- dependencias.bat
+++ dependencias.bat
@@ -3,4 +3,6 @@ python -m pip install mysql-connector-python
 pip install openpyxl
 pip install tkcalendar
 pip install pyexcel pyexcel-xls pyexcel-xlsx
+pip install Gitpython
 python main.py
+wscript.exe "invisible.vbs" "dependencias.bat"
--- inicio.bat
+++ inicio.bat
@@ -1,1 +1,7 @@
-wscript.exe "invisible.vbs" "dependencias.bat"
+python -m pip install pillow
+python -m pip install mysql-connector-python
+pip install openpyxl
+pip install tkcalendar
+pip install pyexcel pyexcel-xls pyexcel-xlsx
+pip install Gitpython
+python main.py
